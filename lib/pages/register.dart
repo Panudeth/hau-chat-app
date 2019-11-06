72542863c0f42ea13c4ed29f490d717909c7950f
Panudeth <panudeth.jar@gmail.com>
2019-11-07 02:35:56 +0700

    add spiner

--- lib/pages/register.dart
+++ lib/pages/register.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 import 'createUser.dart';
 import 'friends.dart';
@@ -13,6 +14,7 @@ class Register extends StatefulWidget {
 }
 
 class _RegisterState extends State<Register> {
+  bool spinner = false;
   String username;
   String password;
   TextEditingController _usernameController = TextEditingController();
@@ -20,6 +22,9 @@ class _RegisterState extends State<Register> {
   final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
 
   void uploadValueToFirebase() async {
+    setState(() {
+      spinner = true;
+    });
     username = _usernameController.text;
     password = _passwordController.text;
     print('user = $username | pass = $password');
@@ -28,6 +33,9 @@ class _RegisterState extends State<Register> {
         .then((user) {
       signIn();
     }).catchError((final error) {
+      setState(() {
+        spinner = false;
+      });
       print('Error => $error');
     });
   }
@@ -39,6 +47,9 @@ class _RegisterState extends State<Register> {
         .then((user) {
       checkAuth(context);
     }).catchError((error) {
+      setState(() {
+        spinner = false;
+      });
       print(' Error=> $error');
     });
   }
@@ -47,9 +58,15 @@ class _RegisterState extends State<Register> {
     FirebaseUser user = await firebaseAuth.currentUser();
     if (user != null) {
       if (user.displayName != null && user.displayName != "") {
+        setState(() {
+          spinner = false;
+        });
         Navigator.pushReplacement(
             context, MaterialPageRoute(builder: (context) => FriendsPage()));
       } else {
+        setState(() {
+          spinner = false;
+        });
         Navigator.pushReplacement(
             context, MaterialPageRoute(builder: (context) => CreateUser(user)));
       }
@@ -62,67 +79,74 @@ class _RegisterState extends State<Register> {
         appBar: AppBar(
           title: Text('Register'),
         ),
-        body: Container(
-          child: Center(
-            child: ListView(
+        body: !spinner
+            ? Container(
+                child: Center(
+                  child: ListView(
 //        mainAxisAlignment: MainAxisAlignment.center,
-              shrinkWrap: true,
-              children: <Widget>[
-                Container(
-                  margin: EdgeInsets.fromLTRB(0, 0, 0, 8),
-                  child: Image.asset(
-                    'images/love.png',
-                    height: 150,
-                    width: 150,
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(50, 10, 50, 5),
-                  child: TextField(
-                    style: Theme.of(context).textTheme.title,
-                    decoration: InputDecoration(
-                        contentPadding: EdgeInsets.all(8.0),
-                        labelText: 'Email',
-                        border: OutlineInputBorder(),
-                        isDense: true),
-                    controller: _usernameController,
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(50, 5, 50, 10),
-                  child: TextField(
-                    style: Theme.of(context).textTheme.title,
-                    decoration: InputDecoration(
-                        contentPadding: EdgeInsets.all(8.0),
-                        labelText: 'Password',
-                        border: OutlineInputBorder(),
-                        isDense: true),
-                    controller: _passwordController,
-                    obscureText: true,
-                  ),
-                ),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: <Widget>[
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(50, 0, 50, 0),
-                      child: ButtonTheme(
-                        minWidth: double.infinity,
-                        child: RaisedButton(
-                          child: Text('Register'),
-                          color: Color(0xffEC5569),
-                          onPressed: () {
-                            uploadValueToFirebase();
-                          },
+                    shrinkWrap: true,
+                    children: <Widget>[
+                      Container(
+                        margin: EdgeInsets.fromLTRB(0, 0, 0, 8),
+                        child: Image.asset(
+                          'images/love.png',
+                          height: 150,
+                          width: 150,
                         ),
                       ),
-                    ),
-                  ],
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(50, 10, 50, 5),
+                        child: TextField(
+                          style: Theme.of(context).textTheme.title,
+                          decoration: InputDecoration(
+                              contentPadding: EdgeInsets.all(8.0),
+                              labelText: 'Email',
+                              border: OutlineInputBorder(),
+                              isDense: true),
+                          controller: _usernameController,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(50, 5, 50, 10),
+                        child: TextField(
+                          style: Theme.of(context).textTheme.title,
+                          decoration: InputDecoration(
+                              contentPadding: EdgeInsets.all(8.0),
+                              labelText: 'Password',
+                              border: OutlineInputBorder(),
+                              isDense: true),
+                          controller: _passwordController,
+                          obscureText: true,
+                        ),
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(50, 0, 50, 0),
+                            child: ButtonTheme(
+                              minWidth: double.infinity,
+                              child: RaisedButton(
+                                child: Text('Register'),
+                                color: Color(0xffEC5569),
+                                onPressed: () {
+                                  uploadValueToFirebase();
+                                },
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Center(child: Text('2019\u00a9 Panudeth Jaruwong'))
+                    ],
+                  ),
                 ),
-                Center(child: Text('2019\u00a9 Panudeth Jaruwong'))
-              ],
-            ),
-          ),
-        ));
+              )
+            : Center(
+                child: SpinKitPumpingHeart(
+                  color: Colors.white,
+                  size: 50.0,
+                ),
+              ));
   }
 }
